59324121aa184e4f0571a9a87790ef1a9ddbe759
JUNYEONG.YOO <woody.chiz@gmail.com>
2017-01-08 20:02:36 +0900

    Add image assets and create main view and its controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordStatusLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    @IBAction func recordButtonAction(_ sender: Any) {
+        recordStatusLabel.text = "Record in Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    
+    @IBAction func stopRecordingButtonAction(_ sender: Any) {
+        recordStatusLabel.text = "Tab to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
