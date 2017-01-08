b836e2e1b9a0705e7645162c3d27a984af600588
JUNYEONG.YOO <woody.chiz@gmail.com>
2017-01-09 01:39:20 +0900

    Add another view controller, implements AVAudioRecorderDelegate so main view controller could trigger segue

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by JUNYEONG.YOO on 1/8/17.
+//  Copyright © 2017 Boostcamp. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordStatusLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    @IBAction func recordButtonAction(_ sender: Any) {
+        recordStatusLabel.text = "Recording in Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        // An audio session is the intermediary between your app and iOS used to configure your app’s audio behavior.
+        let session = AVAudioSession.sharedInstance()
+        
+        // AVAudioSessionCategoryPlayAndRecord is the category for recording (input) and playback (output) of audio, 
+        // such as for a VoIP (Voice over Internet Protocol) app.
+        // AVAudioSessionCategoryOptions.defaultToSpeaker routes audio from the session to the built-in speaker by default.
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true  // Why metering is on? Normally it is set to off because it uses resources.
+        audioRecorder.prepareToRecord()         // Creates an audio file and prepares the system for recording.
+        audioRecorder.record()                  // Starts or resumes recording.
+    }
+    
+    @IBAction func stopRecordingButtonAction(_ sender: Any) {
+        recordStatusLabel.text = "Tab to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        
+        audioRecorder.stop()
+        let session = AVAudioSession.sharedInstance()
+        try! session.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            print(audioRecorder.url)
+            performSegue(withIdentifier: "PlaybackSegue", sender: audioRecorder.url)
+        } else {
+            print("Recording was not successful")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PlaybackSegue" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+}
+
